75249ee207040cc4746c0edfe2a2e0705a4361e7
Boriss Mejias <bmejias@dcc.uchile.cl>
2016-09-23 17:43:31 +0200

    Collecting pool information through jmx rmi

--- alfgard.py
+++ alfgard.py
@@ -2,8 +2,9 @@
 
 from time import sleep
 import io
-import sys
 import psycopg2
+import subprocess
+import sys
 
 try:
     import configparser
@@ -38,8 +39,29 @@ def check_db_connections(cursor, dbname, pmin, pmax):
     return (c, ratio)
 
 
-def get_pool_size(username, password):
-    return 42
+def get_pool_size(config):
+    bean = "Alfresco:Name=ConnectionPool"
+    props = "NumActive NumIdle"
+    host = config['jmx']['host']
+    port = config['jmx']['port']
+    url = "service:jmx:rmi:///jndi/rmi://%s:%s/alfresco/jmxrmi" % (host, port)
+
+    jmx_vars = {'java': config['jmx']['java'],
+                'jmxterm': config['jmx']['jmxterm'],
+                'url': url,
+                'user': config['jmx']['user'],
+                'password': config['jmx']['password']}
+    get_values = 'echo "get -s -b %s %s"' % (bean, props)
+    jmx_call = "%(java)s -jar %(jmxterm)s" % jmx_vars
+    jmx_call += " -l %(url)s" % jmx_vars
+    jmx_call += " -u %(user)s -p %(password)s -v silent -n" % jmx_vars
+    call = '%s | %s' % (get_values, jmx_call)
+    p = subprocess.Popen(call, shell=True, stdout=subprocess.PIPE).stdout.read()
+    result = p.decode('UTF-8')
+    values = result.split('\n')
+    active = int(values[0])
+    idle = int(values[1])
+    return (active, idle, active + idle)
 
 
 def main():
@@ -48,15 +70,19 @@ def main():
     cursor = connect_to_db(config)
 
     db_stream = io.open(config['output']['db'], 'w')
+    db_stream.write("MIN\tCURR\tMAX\t%\tACT\tIDLE\tPOOL\tDIFF\n")
+    db_stream.flush()
     while True:
         dbname = config['db']['name']
         pmin = config['db']['poolmin']
         pmax = config['db']['poolmax']
         (c, ratio) = check_db_connections(cursor, dbname, pmin, pmax)
-        pool = get_pool_size(config['jmx']['user'], config['jmx']['password'])
+        (active, idle, pool) = get_pool_size(config)
         relation = c - pool
-        db_stream.write("%s;%s;%s;%.1f;%s;%s\n" % (pmin, c, pmax, ratio, pool,
-                                                   relation))
+        db_stream.write("%s\t%s\t%s\t%.1f\t%s\t%s\t%s\t%s\n" % (pmin, c, pmax,
+                                                                ratio, active,
+                                                                idle, pool,
+                                                                relation))
         db_stream.flush()
         sleep(2)
 
